d71a353544666fb7f131b82b9fa0e40cd992f64d
physicsb0y <sagar1.735401@trc.tu.edu.np>
2023-06-12 11:09:43 +0545

    Login and Register Manual completed

--- carzone/settings.py
+++ carzone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'pages.apps.PagesConfig',
     'cars.apps.CarsConfig',
+    'accounts.apps.AccountsConfig',
 
     'ckeditor',
     'django.contrib.humanize',
@@ -137,3 +138,9 @@ MEDIAFILES_DIRS = [BASE_DIR / 'media']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
